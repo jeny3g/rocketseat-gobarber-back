c6a24d23282862016a1a66ad963ad826f900ce68
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2021-01-17 00:12:10 -0300

    Rescrevendo repositorios

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,7 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
+import ICreateAppointmentDto from '../dtos/ICreateAppointmentDTO';
 
 export default interface IAppointmentsRepository {
+  create(data: ICreateAppointmentDto): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
