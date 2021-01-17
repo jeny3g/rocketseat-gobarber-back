5a35356326ae238420b4bc9ccb6853199c21059e
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2021-01-17 04:11:49 -0300

    Pensando nos testes

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -2,10 +2,10 @@ import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
   it('should be able to create a new Appointment', () => {
-    expect(1 + 2).toBe(3);
+
   });
 
-  it('should not be able to create two Appointment on the same time', () => {
-    expect(1 + 2).toBe(3);
-  });
+  // it('should not be able to create two Appointment on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
 });
