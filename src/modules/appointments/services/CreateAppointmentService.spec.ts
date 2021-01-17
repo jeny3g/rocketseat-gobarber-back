d0f1c8650c21512a74bb99afdf095d11af3bf119
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2021-01-17 05:12:36 -0300

    Testes de agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -14,10 +16,27 @@ describe('CreateAppointment', () => {
     });
 
     expect(appointment).toHaveProperty('id');
-    expect(appointment.provider_id).toBe('1933723a2c9279df9a4c13b1aa6b72bc')
+    expect(appointment.provider_id).toBe('1933723a2c9279df9a4c13b1aa6b72bc');
   });
 
-  // it('should not be able to create two Appointment on the same time', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('should not be able to create two Appointment on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '1933723a2c9279df9a4c13b1aa6b72bc',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '1933723a2c9279df9a4c13b1aa6b72bc',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
