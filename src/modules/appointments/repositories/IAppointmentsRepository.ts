512dd857268b5a7f3dd2a9b67ab93cfb628bac85
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2021-01-23 22:33:45 -0300

    Listando horarios disponiveis

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,11 +1,15 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDto from '../dtos/ICreateAppointmentDTO';
-import IFindAllInMonthFromProviderDto from '../dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO copy';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDto): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(
-    data: IFindAllInMonthFromProviderDto,
+    data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
+  findAllInDayFromProvider(
+    data: IFindAllInDayFromProviderDTO,
   ): Promise<Appointment[]>;
 }
