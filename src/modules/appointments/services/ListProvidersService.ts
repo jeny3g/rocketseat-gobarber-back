570a2676f2945c1c641ce0330a9716db5a4d7e0d
SenhorBiscoito <mirandajean2009@gmail.com>
2021-03-03 21:37:26 -0300

    Lista de providers com serialização

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import User from '@modules/users/infra/typeorm/entities/User';
@@ -29,7 +30,7 @@ class ListProvidersService {
       });
     }
 
-    await this.cacheProvider.save(`providers-list:${user_id}`, users);
+    await this.cacheProvider.save(`providers-list:${user_id}`, classToClass(users));
 
     return users;
   }
